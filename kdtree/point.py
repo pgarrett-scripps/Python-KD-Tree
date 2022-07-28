63e0374863cf9878c43185dc26feb6e9e6210dc3
pgarrett <pgarrett@scripps.edu>
2022-07-27 19:30:16 -0700

    updated point object, added pickle support

--- kdtree/point.py
+++ kdtree/point.py
@@ -1,10 +1,4 @@
 class Point(list):
-    def __new__(cls, value, name=None, values=None):
-        s = super(Point, cls).__new__(cls, value)
-        return s
-
-    @staticmethod
-    def create(values, data):
-        point = Point(values)
-        point.data = data
-        return point
+    def __init__(self, value):
+        super().__init__(value)
+        data = None
